80c55513e6a142c9fb219dcae165564067894c01
uneiz1103 <uneizshaikh1103@gmail.com>
2025-07-24 23:20:25 +0530

    Update Student model with optional age field

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Student(BaseModel):
     
     name: str = 'uneiz'
+    age: Optional[int] = None
 
-new_student = {}
+new_student = {'age': 24}
 
 student = Student(**new_student)
 
