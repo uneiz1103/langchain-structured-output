e9f8371f8e9cc38639f2d329ee28e3042590bea1
uneiz1103 <uneizshaikh1103@gmail.com>
2025-07-24 23:27:45 +0530

    Enhance Student model with EmailStr for email validation

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 class Student(BaseModel):
     
     name: str = 'uneiz'
     age: Optional[int] = None
+    email: EmailStr
 
-new_student = {'age': 24}
+new_student = {'age': '24', 'email': 'abc@gmail.com'}
 
 student = Student(**new_student)
 
