8cb9313e2d77f8a1833586cd3a66b18ad41607c0
uneiz1103 <uneizshaikh1103@gmail.com>
2025-07-25 15:31:03 +0530

    Add cgpa metadata and demonstrate model export to dict and JSON

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -6,14 +6,17 @@ class Student(BaseModel):
     name: str = 'uneiz'
     age: Optional[int] = None
     email: EmailStr
-    cgpa : float = Field(gt=0, lt=10)
+    cgpa : float = Field(gt=0, lt=10, description="A decimal value representing the CGPA of the student")
 
 new_student = {'age':24, 'email': 'abc@gmail.com', 'cgpa': 5}
 
 
 student = Student(**new_student)
 
-print(student.name)
-print(student.age)
-print(student.email)
-print(student.cgpa)
+student_dict = dict(student)
+
+student_json = student.model_dump_json()
+
+print(student)
+print(student_dict)
+print(student_json)
